3ce7aba76254a81d87a6213e6f8033bec05fe106
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-08-30 20:55:20 +0200

    Add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { CssBaseline } from "@mui/material";
+import { createTheme, StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement!);
+
+const theme = createTheme({
+    components: {
+        MuiPopover: {
+            defaultProps: {
+                container: rootElement
+            }
+        },
+        MuiPopper: {
+            defaultProps: {
+                container: rootElement
+            }
+        }
+    }
+});
+
+root.render(
+    <React.StrictMode>
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
+        </StyledEngineProvider>
+    </React.StrictMode>
+);
